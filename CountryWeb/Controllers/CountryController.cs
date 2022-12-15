255515af8b8ab1e670a97af4a021986204a9d734
AsilbekAbdulhamidov <104564092+AsilbekAbdulhamidov@users.noreply.github.com>
2022-12-15 18:07:05 +0500

    funksianllik crud ishladi,

--- CountryWeb/Controllers/CountryController.cs
+++ CountryWeb/Controllers/CountryController.cs
@@ -3,6 +3,7 @@ using Data1.DTO;
 using Microsoft.AspNetCore.Mvc;
 using ToWinFromFromAPI;
 using CountryWeb.ViewModels;
+using System.Diagnostics.Metrics;
 
 namespace CountryWeb.Controllers
 {
@@ -15,7 +16,7 @@ namespace CountryWeb.Controllers
             halper = new HalperAPI("https://localhost:7296/api/Country");
         }
 
-        public async  Task<IActionResult> Index()
+        public async Task<IActionResult> Index()
         {
             var res = await halper.Get();
             return View(res);
@@ -29,25 +30,25 @@ namespace CountryWeb.Controllers
 
         public async Task<IActionResult> Add(CounrtyDto countryDto)
         {
-            await halper.Post(countryDto);    
+            await halper.Post(countryDto);
             return Redirect("Index");
         }
         //Put  Edit
         public async Task<IActionResult> Edit(int id)
         {
             var res = await halper.Get(id);
-            
-            
+
+
             return View(res);
         }
         public async Task<IActionResult> Save(Country country)
         {
             int id = country.Id;
             CounrtyDto dto = new CounrtyDto()
-            { 
+            {
                 Description = country.Description,
-                MspUrl= country.MspUrl,
-                Name= country.Name,
+                MspUrl = country.MspUrl,
+                Name = country.Name,
 
             };
             halper.Put(id, dto);
@@ -57,8 +58,50 @@ namespace CountryWeb.Controllers
         //Show
         public async Task<IActionResult> Show(int? id)
         {
-            var res = await halper.Get(id??1);
+            var res = await halper.Get(id ?? 1);
             return View(res);
         }
+
+        //Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var res = await halper.Get(id ?? 1);
+            CounrtyDto dto = new CounrtyDto()
+            {
+                Description = res.Description,
+                MspUrl = res.MspUrl,
+                Name = res.Name,
+
+            };
+            CountryEditModel editModel = new CountryEditModel()
+            {
+                Id = id ?? 1,
+                Dto = dto,
+
+            };
+            return View(editModel);
+        }
+
+        public async Task<IActionResult> DelSave(int? id)
+        {
+            int Id = id ?? 1;
+             await halper.Delete(Id);
+            return RedirectToAction("index");
+        }
+        //delete
+        /* public async Task<IActionResult> Delete(int id)
+         {
+             var  res = halper.Get(id);
+             if (res != null)
+             {
+                await halper.Delete(id);
+                 return RedirectToAction("index");
+             }
+             else
+             {
+                 return RedirectToAction("CountryNotFound", new { Id=id });
+             }
+
+         }*/
     }
 }
